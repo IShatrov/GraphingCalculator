c702db48c7bdc557e031d4217dcd745364081b3e
IShatrov <shatrov2004@mail.ru>
2023-11-11 18:31:11 +0300

    Added constructor to BinaryOperatorNode

--- src/main/java/node/BinaryOperatorNode.java
+++ src/main/java/node/BinaryOperatorNode.java
@@ -1,7 +1,11 @@
 package node;
 
 public class BinaryOperatorNode extends ExpressionNode {
-    BinaryOperator op;
+    private final BinaryOperator op;
+
+    BinaryOperatorNode(BinaryOperator op) {
+        this.op = op;
+    }
 
     double calculate(double x) {
         return op.calculation.applyAsDouble(left.calculate(x), right.calculate(x));
