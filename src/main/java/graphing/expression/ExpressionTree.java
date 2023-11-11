d5e0437213910b601645ae668e4176d348b50244
IShatrov <shatrov2004@mail.ru>
2023-11-11 20:52:49 +0300

    Added ExpressionTree class

--- src/main/java/graphing/expression/ExpressionTree.java
+++ src/main/java/graphing/expression/ExpressionTree.java
@@ -1,4 +1,13 @@
 package graphing.expression;
 
 public class ExpressionTree {
+    private final ExpressionNode root;
+
+    public ExpressionTree(ExpressionNode root) {
+        this.root = root;
+    }
+
+    public double calculate(double x) {
+        return root.calculate(x);
+    }
 }
