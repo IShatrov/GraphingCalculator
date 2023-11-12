3a741abc88cd57f54976c105d21f736db82aa36f
IShatrov <shatrov2004@mail.ru>
2023-11-12 15:23:49 +0300

    TabulatedFunction.java

---
+++ src/main/java/graphing/plotter/TabulatedFunction.java
@@ -0,0 +1,13 @@
+package graphing.plotter;
+
+import java.util.ArrayList;
+
+public class TabulatedFunction {
+    private static final int MAX_POINTS = 10_000;
+
+    private ArrayList<Double> xValues, yValues;
+
+    public TabulatedFunction() {
+        
+    }
+}
