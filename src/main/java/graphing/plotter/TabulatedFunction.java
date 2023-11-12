2a2ba20af69bff7b53c62aa9234430c077619517
IShatrov <shatrov2004@mail.ru>
2023-11-12 16:51:20 +0300

    Finished TabulatedFunction

--- src/main/java/graphing/plotter/TabulatedFunction.java
+++ src/main/java/graphing/plotter/TabulatedFunction.java
@@ -1,13 +1,41 @@
 package graphing.plotter;
 
+import graphing.expression.ExpressionTree;
 import java.util.ArrayList;
 
 public class TabulatedFunction {
     private static final int MAX_POINTS = 10_000;
 
-    private ArrayList<Double> xValues, yValues;
+    private final ArrayList<Double> xValues, yValues;
+    private final double xMin, xMax, yMin, yMax;
 
-    public TabulatedFunction() {
-        
+    public TabulatedFunction(ExpressionTree function, double xMin, double xMax, double yMin, double yMax)
+            throws ArithmeticException {
+        if (xMin >= xMax) {
+            throw new ArithmeticException("xMin larger than xMax");
+        }
+
+        if (yMin >= yMax) {
+            throw new ArithmeticException("yMin larger than yMax");
+        }
+
+        this.xMin = xMin;
+        this.xMax = xMax;
+        this.yMin = yMin;
+        this.yMax = yMax;
+
+        xValues = new ArrayList<>();
+        yValues = new ArrayList<>();
+
+        double delta = (xMax - xMin) / MAX_POINTS;
+
+        for (double x = xMin; x <= xMax; x += delta) {
+            double y = function.calculate(x);
+
+            if (Double.isFinite(y)) {
+                xValues.add(x);
+                yValues.add(y);
+            }
+        }
     }
 }
