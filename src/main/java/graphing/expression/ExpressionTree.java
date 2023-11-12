0c1cf55bdce233908a9a44e9733cb9ac4c7dbd95
IShatrov <shatrov2004@mail.ru>
2023-11-12 16:33:18 +0300

    Added documentation comments to ExpressionTree

--- src/main/java/graphing/expression/ExpressionTree.java
+++ src/main/java/graphing/expression/ExpressionTree.java
@@ -3,17 +3,34 @@ package graphing.expression;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * An abstract syntax tree for math expressions.
+ */
 public class ExpressionTree {
     private final ExpressionNode root;
 
+    /**
+     * Constructs ExpressionTree with given root.
+     * @param root node to set as root
+     */
     public ExpressionTree(ExpressionNode root) {
         this.root = root;
     }
 
+    /**
+     * Calculates function at given point.
+     * @param x point to calculate at
+     * @return
+     */
     public double calculate(double x) {
         return root.calculate(x);
     }
 
+    /**
+     * Generates graphviz log of the tree.
+     * @param filename name of file
+     * @throws IOException when writing fails
+     */
     public void graphvizLog(String filename) throws IOException {
         FileWriter writer = new FileWriter(filename);
 
