05436e5f96a8cc3ecfdb17bd256449d724bed2a8
IShatrov <shatrov2004@mail.ru>
2023-11-16 20:59:23 +0300

    Replaced concatenation with string block in PlotBuilder.java

--- src/main/java/graphing/plotter/PlotBuilder.java
+++ src/main/java/graphing/plotter/PlotBuilder.java
@@ -18,17 +18,20 @@ public class PlotBuilder {
                         "y = [" + func.yString(", ")
                     );
 
-        writer.write("]\n" +
-                        "fig.plot(list(x), list(y), linestyle=\"None\",  marker=\".\", markersize=1)\n" +
-                        "fig.spines['left'].set_position('zero')\n" +
-                        "fig.spines['bottom'].set_position('zero')\n" +
-                        "fig.spines['right'].set_color('none')\n" +
-                        "fig.spines['top'].set_color('none')\n" +
-                        "fig.xaxis.set_ticks_position('bottom')\n" +
-                        "fig.yaxis.set_ticks_position('left')\n" +
-                        "fig.set_xlim([" + func.getxMin() + ", " + func.getxMax() + "])\n" +
-                        "fig.set_ylim([" + func.getyMin() + ", " + func.getyMax() + "])\n" +
-                        "plt.show()"
+        writer.write("""
+                        ]
+                        fig.plot(list(x), list(y), linestyle="None",  marker=".", markersize=1)
+                        fig.spines['left'].set_position('zero')
+                        fig.spines['bottom'].set_position('zero')
+                        fig.spines['right'].set_color('none')
+                        fig.spines['top'].set_color('none')
+                        fig.xaxis.set_ticks_position('bottom')
+                        fig.yaxis.set_ticks_position('left')
+                        """
                     );
+
+        writer.write("fig.set_xlim([" + func.getxMin() + ", " + func.getxMax() + "])\n" +
+                        "fig.set_ylim([" + func.getyMin() + ", " + func.getyMax() + "])\n" +
+                        "plt.show()");
     }
 }
